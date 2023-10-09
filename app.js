b3e363d70c99cc344120fa7c9207b80c9ee5b0b3
tviv <697668@gmail.com>
2023-10-09 13:45:20 +1000

    fix log for requests for old node js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-	console.log(new Date().toISOString(), 'request', req.url, req.socket?.remoteAddress)
+	console.log(new Date().toISOString(), 'request', req.url, req.socket && req.socket.remoteAddress)
 	next()
 })
 
